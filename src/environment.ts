26e7b94b7f3a9223e7eae1e5b43bf3c37b9a25f3
Alessandro <fimaja.9@gmail.com>
2025-07-29 18:24:25 +0200

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -5,6 +5,10 @@ export const environment = {
     api_url: 'http://192.168.1.64:8080/',
     ws_uri: 'http://192.168.1.64:8080/',
 
+    // api_url: 'http://93.42.111.232:8080/',
+    // ws_uri: 'http://93.42.111.232:8080/',
+
     // host: 'localhost',
     host: '192.168.1.64',
+    // host: '93.42.111.232'
   };
